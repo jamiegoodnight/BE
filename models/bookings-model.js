baef972ef2f3be69edfa3b37ef4ce3f488d77fb9
Jamie Goodnight <goodnightlambda@gmail.com>
2019-08-28 16:55:08 -0400

    Fixed get bookings by user

--- models/bookings-model.js
+++ models/bookings-model.js
@@ -1,56 +1,110 @@
-const db = require('../db/dbConfig.js');
+const db = require("../db/dbConfig.js");
 
 module.exports = {
-    add,
-    find,
-    findBy,
-    findById,
-    remove,
-    update
-  };
+  add,
+  find,
+  findBy,
+  findById,
+  remove,
+  update,
+  getBookingsForUser
+};
 
-  function find() {
-    return db('bookings').select('*');
-  }
+function find() {
+  return db("bookings");
+}
 
-  function add(booking) {
-    return db('bookings')
-    .insert(booking, 'booking_id')
-      .then(ids => {
-        // console.log("DID we get here?", ids)
-        const [booking_id] = ids;
-        return findById(booking_id);
-      });
-  }
+function add(booking) {
+  return db("bookings")
+    .insert(booking, "booking_id")
+    .then(ids => {
+      // console.log("DID we get here?", ids)
+      const [booking_id] = ids;
+      return findById(booking_id);
+    });
+}
 
-  function findBy(filter) {
-    return db('bookings').where(filter)
-  }
+// function findBy(id) {
+//   return db("bookings").where({ booking_id: id });
+// }
 
-  function findById(id) {
-    return db('bookings')
-      .where({ booking_id: id })
-      .first();
-  }
+function findById(id) {
+  return db("bookings")
+    .where({ booking_id: id })
+    .first();
+}
 
-  function remove(id) {
-    return db('bookings')
-      .where({booking_id: id})
-      .del()
-  }
+function remove(id) {
+  return db("bookings")
+    .where({ booking_id: id })
+    .del();
+}
 
-  function update(data, id){
-    return db('bookings')
-      .where({booking_id: id})
-      .update(data)
-  }
+function update(data, id) {
+  return db("bookings")
+    .where({ booking_id: id })
+    .update(data);
+}
 
-  // function findBy(filter) {
-  //   return db('bookings as b')
-  //   .select('b.booking_id', 'b.user_id', 'u.username', 'b.listing_id', 'l.listing_name', 'b.startDate', 'b.stopDate')
-  //   .join('users as u', 'b.user_id', 'u.id')
-  //   .join('listings as l', 'l.listing_id', 'b.listing_id')
-  //   .where(filter)
+// function findBy(id) {
+//   return db("bookings as b")
+//     .where({ id })
+//     .select(
+//       "b.booking_id",
+//       "b.user_id",
+//       "u.username",
+//       "b.listing_id",
+//       "l.listing_name",
+//       "b.startDate",
+//       "b.stopDate"
+//     )
+//     .join("users as u", "b.user_id", "u.id")
+//     .join("listings as l", "l.listing_id", "b.listing_id");
+// }
 
+function findBy(id) {
+  return db("users")
+    .where({ id })
+    .select("users.username")
+    .first();
+}
 
-  // }
+function getBookingsForUser(id) {
+  return (
+    db("bookings as b")
+      .leftJoin("listings as l", "b.listing_id", "l.listing_id")
+      // .select({
+      //   booking_id: "b.booking_id",
+      //   userr_id: "b.user_id",
+      //   listing_name: "l.listing_name",
+      //   listing_id: "l.listing_id",
+      //   startDate: "b.startDate",
+      //   stopDate: "b.stopDate"
+      // })
+      .select(
+        "b.booking_id",
+        "b.user_id",
+        "b.listing_id",
+        "l.listing_name",
+        "b.startDate",
+        "b.stopDate"
+      )
+      .then(bookings => {
+        return bookings.filter(e => e.user_id === parseInt(id));
+      })
+  );
+
+  return db("bookings as b").where({ user_id: id });
+  // .select(
+  //   "b.booking_id",
+  //   // "b.user_id",
+  //   "b.listing_id",
+  //   "l.listing_name",
+  //   "b.startDate",
+  //   "b.stopDate"
+  // );
+}
+
+function getListingForBooking(id) {
+  return db("listings").where();
+}
--- routes/listingRouter.js
+++ routes/listingRouter.js
@@ -1,113 +1,178 @@
-const router = require('express').Router();
+const router = require("express").Router();
 
-const Bookings = require('../models/bookings-model.js');
-const Listings = require('../models/listings-model.js');
-const restricted = require('../middleware/restricted.js');
+const Bookings = require("../models/bookings-model.js");
+const Listings = require("../models/listings-model.js");
+const restricted = require("../middleware/restricted.js");
 
 // ----- Get all listings ------
-router.get('/', (req, res) => {
-    Listings.find()
-      .then(listing => {
-        res.status(200).json(listing);
-      })
-      .catch(err => res.status(400).send(err));
-  });
+router.get("/", (req, res) => {
+  Listings.find()
+    .then(listing => {
+      res.status(200).json(listing);
+    })
+    .catch(err => res.status(400).send(err));
+});
 
 // ------ Add new Listing ---------
-router.post('/', restricted, (req,res) =>{
-    let listing = req.body;
-    console.log("listing in body", listing)
-    !listing.listing_name || !listing.user_id ? res.status(400).json({message: "A new Listing must had a listing_name and a user_id property."}) :
-    Listings.add(listing)
-      .then(listing => {
-        let {listing_id, listing_name, created_at} = listing
-        res.status(201).json({message: "Successfully Created a Listing", listing_id, listing_name, created_at});
+router.post("/", restricted, (req, res) => {
+  let listing = req.body;
+  console.log("listing in body", listing);
+  !listing.listing_name || !listing.user_id
+    ? res.status(400).json({
+        message: "A new Listing must had a listing_name and a user_id property."
       })
-      .catch( err => {
-        res.status(400).json({message: "Oh no, something went wrong! Make sure the listing name is Unique. If it is, speak with BE."});
-      })
-
-})
+    : Listings.add(listing)
+        .then(listing => {
+          let { listing_id, listing_name, created_at } = listing;
+          res.status(201).json({
+            message: "Successfully Created a Listing",
+            listing_id,
+            listing_name,
+            created_at
+          });
+        })
+        .catch(err => {
+          res.status(400).json({
+            message:
+              "Oh no, something went wrong! Make sure the listing name is Unique. If it is, speak with BE."
+          });
+        });
+});
 
 // ----- Remove a Listing --------
-router.delete('/:id', restricted, (req,res)=> {
-   const id = req.params.id
+router.delete("/:id", restricted, (req, res) => {
+  const id = req.params.id;
 
-    Listings.remove(id)
-  .then( deleted => {
-    !deleted ? res.status(400).json({message: "That Listing does not exist."}) :
-    res.status(200).json({message: `Successfully deleted listing with an ID of ${id}`})
-  })
-  .catch(err => {
-    res.status(500).json({message: "There was a problem in the server with your request."})
-  })
-})
+  Listings.remove(id)
+    .then(deleted => {
+      !deleted
+        ? res.status(400).json({ message: "That Listing does not exist." })
+        : res.status(200).json({
+            message: `Successfully deleted listing with an ID of ${id}`
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem in the server with your request."
+      });
+    });
+});
 
 // ---- Update a Listing ------
-router.put('/:id', restricted, (req,res)=>{
-  const id = req.params.id
-  const listing = req.body
+router.put("/:id", restricted, (req, res) => {
+  const id = req.params.id;
+  const listing = req.body;
 
   Listings.update(listing, id)
     .then(updated => {
-      !updated ? res.status(400).json({message: "That Listing does not exist."}) :
-      res.status(200).json({message: `Successfully updated listing with an ID of ${id}.`})
+      !updated
+        ? res.status(400).json({ message: "That Listing does not exist." })
+        : res.status(200).json({
+            message: `Successfully updated listing with an ID of ${id}.`
+          });
     })
     .catch(err => {
-      res.status(400).json({message: "You need to pass in the data you want to update."})
-    })
-})
+      res
+        .status(400)
+        .json({ message: "You need to pass in the data you want to update." });
+    });
+});
 
 // ----- Get a Listing by Id -----
-router.get('/:id', (req,res)=> {
-  const id = req.params.id
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
 
   Listings.findById(id)
-    .then( listing => {
-      !listing ? res.status(400).json({message: "That listing does not exist."}) :
-      console.log("HERES THE FRICKIN ID", id)
-      Bookings.findBy({listing_id: id})
-      .then(bookings => {
-        res.status(200).json({...listing, bookings})
-      })
-      .catch( err => {
-        res.status(500).json({err, message: "In Bookings data model"})
-      })
-
+    .then(listing => {
+      !listing
+        ? res.status(400).json({ message: "That listing does not exist." })
+        : console.log("HERES THE FRICKIN ID", id);
+      Bookings.findBy({ listing_id: id })
+        .then(bookings => {
+          res.status(200).json({ ...listing, bookings });
+        })
+        .catch(err => {
+          res.status(500).json({ err, message: "In Bookings data model" });
+        });
     })
-    .catch( err => {
-      res.status(500).json({message: "Error happened in the server", err})
-    })
-})
+    .catch(err => {
+      res.status(500).json({ message: "Error happened in the server", err });
+    });
+});
 
 // ----- Add a booking for the current Listing -----
-router.post('/:id/booking', restricted, (req,res)=> {
-    let dates = req.body;
-    let listing_id = Number(req.params.id);
-    let user_id = req.userId;
-    !dates.startDate || !dates.stopDate ? res.status(400).json({message: "A new Booking must have a startDate and a stopDate."}) :
-    Bookings.add({listing_id, user_id, ...dates})
-      .then(booking => {
-        // let {listing_id, listing_name, created_at} = listing
-        res.status(201).json({message: "Successfully Created a Booking", booking});
+router.post("/:id/booking", restricted, (req, res) => {
+  let dates = req.body;
+  let listing_id = Number(req.params.id);
+  let user_id = req.userId;
+  !dates.startDate || !dates.stopDate
+    ? res.status(400).json({
+        message: "A new Booking must have a startDate and a stopDate."
       })
-      .catch( err => {
-        res.status(400).json({message: "Uh oh! Make sure the listing exists to make a booking."});
-      })
-})
+    : Bookings.add({ listing_id, user_id, ...dates })
+        .then(booking => {
+          // let {listing_id, listing_name, created_at} = listing
+          res
+            .status(201)
+            .json({ message: "Successfully Created a Booking", booking });
+        })
+        .catch(err => {
+          res.status(400).json({
+            message: "Uh oh! Make sure the listing exists to make a booking."
+          });
+        });
+});
 
 // ----- Delete a Booking -----
-router.delete('/:listing_id/booking/:booking_id', restricted, (req,res)=> {
-  const id = req.params.booking_id
+router.delete("/:listing_id/booking/:booking_id", restricted, (req, res) => {
+  const id = req.params.booking_id;
 
-   Bookings.remove(id)
- .then( deleted => {
-   !deleted ? res.status(400).json({message: "That Listing does not exist."}) :
-   res.status(200).json({message: `Successfully deleted Booking with an ID of ${id}`})
- })
- .catch(err => {
-   res.status(500).json({message: "There was a problem in the server with your request."})
- })
-})
+  Bookings.remove(id)
+    .then(deleted => {
+      !deleted
+        ? res.status(400).json({ message: "That Listing does not exist." })
+        : res.status(200).json({
+            message: `Successfully deleted Booking with an ID of ${id}`
+          });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "There was a problem in the server with your request."
+      });
+    });
+});
 
-  module.exports = router;
+// Find a booking
+
+// ----- Add a booking for the current Listing -----
+
+router.get("/booking/:id", (req, res) => {
+  let id = req.params.id;
+  Bookings.findBy(id)
+    .then(user => {
+      let myuser = { ...user };
+
+      Bookings.getBookingsForUser(id).then(bookings => {
+        myuser.bookings = bookings;
+        res.status(200).json(myuser);
+      });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+module.exports = router;
+
+router.get("/all/booking/", (req, res) => {
+  // let id = req.params.id;
+  Bookings.find()
+    .then(bookings => {
+      res.status(200).json(bookings);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(400).json(err);
+    });
+});
